734dbe0b6c4ffd7c59f8ea6c9bde8d2320d34ecf
robhowley <howley.robert@gmail.com>
2019-02-13 11:55:19 -0500

    lambda deploys

--- _internal/filter_plugins/lobotfilters.py
+++ _internal/filter_plugins/lobotfilters.py
@@ -8,9 +8,14 @@ def snake_keys_to_pascal(some_dict):
     return {snake_to_pascal(k): v for k, v in some_dict.items()}
 
 
+def copy_keys(some_dict, exclude=None):
+    return {k: v for k, v in some_dict.items() if k not in (exclude or [])}
+
+
 class FilterModule(object):
     def filters(self):
         return dict(
+            copy_keys=copy_keys,
             snake_to_pascal=snake_to_pascal,
             snake_keys_to_pascal=snake_keys_to_pascal
         )
