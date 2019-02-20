d1c39827c95c3bdb3b3df59abb6e1037b249b331
Rob Howley <howley.robert@gmail.com>
2019-02-19 21:14:07 -0500

    default flatten for template params (#7)

--- _internal/filter_plugins/lobotfilters.py
+++ _internal/filter_plugins/lobotfilters.py
@@ -16,10 +16,26 @@ def copy_keys(some_dict, include=None, exclude=None):
     return {k: v for k, v in some_dict.items() if should_keep(k)}
 
 
+def flatten_dict(some_dict, delim='_'):
+    def flatten_nested(key, value):
+        if hasattr(value, 'items'):
+            return [(key + delim + k, v) for k, v in flatten_dict(value).items()]
+        else:
+            return [(key, value)]
+
+    return dict([item for k, v in some_dict.items() for item in flatten_nested(k, v)])
+
+
+def prep_template_parameters(some_dict, **kwargs):
+    return snake_keys_to_pascal(flatten_dict(copy_keys(some_dict, **kwargs)))
+
+
 class FilterModule(object):
     def filters(self):
         return dict(
             copy_keys=copy_keys,
+            flatten_dict=flatten_dict,
             snake_to_pascal=snake_to_pascal,
-            snake_keys_to_pascal=snake_keys_to_pascal
+            snake_keys_to_pascal=snake_keys_to_pascal,
+            prep_template_parameters=prep_template_parameters
         )
